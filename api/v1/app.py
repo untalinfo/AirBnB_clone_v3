678621802b74891234e6b525158aa21cd57f815e
untalinfo <racso1607@gmail.com>
2020-09-09 17:06:58 +0000

    fixes pep8 and docstring. In file app.py add new method handler error 404 added

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
-from flask import Flask
+"""
+This module create a blueprint
+"""
+from flask import Flask, make_response, jsonify, Blueprint
 from models import storage
 from api.v1.views import app_views
 
@@ -11,7 +13,22 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
+    """
+    Remove the current session
+    Args:
+        exception :
+    """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    page not found handler error 404
+    Returns:
+        [json]: page with json file
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+This module create a diverse task
+"""
 from api.v1.views import app_views
 from flask import jsonify
 
